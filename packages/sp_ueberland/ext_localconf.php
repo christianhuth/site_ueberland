ca1c0851e4436c453ba70f88e1d301f074e0d4bb
christianhuth <christian@knell.it>
2026-02-12 16:25:48 +0100

    add deployer config

---
+++ packages/sp_ueberland/ext_localconf.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+defined('TYPO3') or die();
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
+        'backendFavicon' => 'EXT:sp_ueberland/Resources/Public/Images/Backend/favicon.png',
+        'backendLogo' => 'EXT:sp_ueberland/Resources/Public/Images/Backend/logo.png',
+        'loginBackgroundImage' => 'EXT:sp_ueberland/Resources/Public/Images/Login/background.jpg',
+        'loginLogo' => 'EXT:sp_ueberland/Resources/Public/Images/Login/logo.svg',
+        'loginLogoAlt' => ''
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['mask'] = [
+        'backend' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Backend/Templates',
+        'backend_layouts_folder' => '',
+        'backendlayout_pids' => '0,1',
+        'content' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Frontend/Templates',
+        'content_elements_folder' => '',
+        'json' => 'EXT:sp_ueberland/Configuration/Mask/mask.json',
+        'layouts' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Frontend/Layouts',
+        'layouts_backend' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Backend/Layouts',
+        'loader_identifier' => 'json',
+        'partials' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Frontend/Partials',
+        'partials_backend' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Backend/Partials',
+        'preview' => 'EXT:sp_ueberland/Resources/Public/Mask/',
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['wv_deepltranslate'] = [
+        'apiKey' => '1eb28236-42b6-da73-2538-2e738e3f9f98:fx'
+];
+
+// to be used in ext_localconf.php
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'] = array_merge(
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'],
+    [
+        'tx_ueberland_ueberland[category]',
+        'tx_ueberland_ueberland[city]',
+        'tx_ueberland_ueberland[country]',
+        'tx_ueberland_ueberland[section]',
+        'tx_ueberland_ueberland[suchtext]'
+    ]
+);
--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -35,22 +35,217 @@ return [
     ],
     'EXTENSIONS' => [
         'backend' => [
-            'backendFavicon' => '',
-            'backendLogo' => '',
-            'loginBackgroundImage' => '',
+            'backendFavicon' => 'EXT:sp_ueberland/Resources/Public/Images/Backend/favicon.png',
+            'backendLogo' => 'EXT:sp_ueberland/Resources/Public/Images/Backend/logo.png',
+            'loginBackgroundImage' => 'EXT:sp_ueberland/Resources/Public/Images/Login/background.jpg',
             'loginFootnote' => '',
             'loginHighlightColor' => '',
-            'loginLogo' => '',
+            'loginLogo' => 'EXT:sp_ueberland/Resources/Public/Images/Login/logo.svg',
             'loginLogoAlt' => '',
         ],
         'extensionmanager' => [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
         ],
+        'gridelements' => [
+            'additionalStylesheet' => '',
+            'disableAutomaticUnusedColumnCorrection' => '0',
+            'disableCopyFromPageButton' => '0',
+            'disableDragInWizard' => '0',
+            'fluidBasedPageModule' => '0',
+            'nestingInListModule' => '0',
+            'overlayShortcutTranslation' => '0',
+        ],
+        'ig_slug' => [
+            'disableOwnMenuItem' => '0',
+        ],
+        'mask' => [
+            'backend' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Backend/Templates',
+            'backend_layouts_folder' => '',
+            'backendlayout_pids' => '0,1',
+            'content' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Frontend/Templates',
+            'content_elements_folder' => '',
+            'json' => 'EXT:sp_ueberland/Configuration/Mask/mask.json',
+            'layouts' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Frontend/Layouts',
+            'layouts_backend' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Backend/Layouts',
+            'loader_identifier' => 'json',
+            'override_shared_fields' => '0',
+            'partials' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Frontend/Partials',
+            'partials_backend' => 'EXT:sp_ueberland/Resources/Private/Extensions/mask/Backend/Partials',
+            'preview' => 'EXT:sp_ueberland/Resources/Public/Mask/',
+        ],
+        'news' => [
+            'advancedMediaPreview' => '1',
+            'archiveDate' => 'date',
+            'categoryBeGroupTceFormsRestriction' => '0',
+            'categoryRestriction' => '',
+            'contentElementRelation' => '1',
+            'dateTimeNotRequired' => '0',
+            'hidePageTreeForAdministrationModule' => '0',
+            'manualSorting' => '0',
+            'prependAtCopy' => '1',
+            'resourceFolderImporter' => '/news_import',
+            'rteForTeaser' => '0',
+            'showAdministrationModule' => '1',
+            'slugBehaviour' => 'unique',
+            'storageUidImporter' => '1',
+            'tagPid' => '1',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+            'showSampleTasks' => '1',
+        ],
+        'static_info_tables' => [
+            'constraints' => [
+                'depends' => [
+                    'extbase' => '11.5.0-11.5.99',
+                    'extensionmanager' => '11.5.0-11.5.99',
+                    'typo3' => '11.5.0-11.5.99',
+                ],
+            ],
+            'enableManager' => '0',
+            'entities' => [
+                'Country',
+                'CountryZone',
+                'Currency',
+                'Language',
+                'Territory',
+            ],
+            'tables' => [
+                'static_countries' => [
+                    'isocode_field' => [
+                        'cn_iso_##',
+                    ],
+                    'label_fields' => [
+                        'cn_short_##' => [
+                            'mapOnProperty' => 'shortName##',
+                        ],
+                        'cn_short_en' => [
+                            'mapOnProperty' => 'shortNameEn',
+                        ],
+                    ],
+                ],
+                'static_country_zones' => [
+                    'isocode_field' => [
+                        'zn_code',
+                        'zn_country_iso_##',
+                    ],
+                    'label_fields' => [
+                        'zn_name_##' => [
+                            'mapOnProperty' => 'name##',
+                        ],
+                        'zn_name_local' => [
+                            'mapOnProperty' => 'localName',
+                        ],
+                    ],
+                ],
+                'static_currencies' => [
+                    'isocode_field' => [
+                        'cu_iso_##',
+                    ],
+                    'label_fields' => [
+                        'cu_name_##' => [
+                            'mapOnProperty' => 'name##',
+                        ],
+                        'cu_name_en' => [
+                            'mapOnProperty' => 'nameEn',
+                        ],
+                    ],
+                ],
+                'static_languages' => [
+                    'isocode_field' => [
+                        'lg_iso_##',
+                        'lg_country_iso_##',
+                    ],
+                    'label_fields' => [
+                        'lg_name_##' => [
+                            'mapOnProperty' => 'name##',
+                        ],
+                        'lg_name_en' => [
+                            'mapOnProperty' => 'nameEn',
+                        ],
+                    ],
+                ],
+                'static_territories' => [
+                    'isocode_field' => [
+                        'tr_iso_##',
+                    ],
+                    'label_fields' => [
+                        'tr_name_##' => [
+                            'mapOnProperty' => 'name##',
+                        ],
+                        'tr_name_en' => [
+                            'mapOnProperty' => 'nameEn',
+                        ],
+                    ],
+                ],
+            ],
+            'version' => '11.5.5',
+        ],
+        'tgm_copyright' => [
+            'copyrightRequired' => '0',
+        ],
+        'wv_deepltranslate' => [
+            'apiKey' => '1eb28236-42b6-da73-2538-2e738e3f9f98:fx',
+        ],
     ],
     'FE' => [
         'cacheHash' => [
             'enforceValidation' => true,
+            'excludedParameters' => [
+                'L',
+                'mtm_campaign',
+                'mtm_keyword',
+                'mtm_kwd',
+                'mtm_source',
+                'mtm_medium',
+                'mtm_content',
+                'mtm_cid',
+                'mtm_group',
+                'mtm_placement',
+                'pk_campaign',
+                'pk_kwd',
+                '_stg_debug',
+                'utm_source',
+                'utm_medium',
+                'utm_campaign',
+                'utm_term',
+                'utm_content',
+                'utm_id',
+                'utm_source_platform',
+                'utm_creative_format',
+                'utm_marketing_tactic',
+                'gtm_debug',
+                '_ga',
+                '_gl',
+                'gad',
+                'gad_source',
+                'gbraid',
+                'gclid',
+                'dclid',
+                'wbraid',
+                'fbclid',
+                'msclkid',
+                'hsa_acc',
+                'hsa_ad',
+                'hsa_cam',
+                'hsa_grp',
+                'hsa_kw',
+                'hsa_mt',
+                'hsa_net',
+                'hsa_src',
+                'hsa_tgt',
+                'hsa_ver',
+                'hsa_ol',
+                'hsa_la',
+                '_hsenc',
+                '_hsmi',
+                '__hssc',
+                '__hstc',
+                '__hsfp',
+                'hsCtaTracking',
+                'submissionGuid',
+            ],
         ],
         'debug' => false,
         'disableNoCacheParameter' => true,
